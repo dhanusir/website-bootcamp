c589f579042e10e5e2e66090b0ae0febf66ea0e2
Cool BRG <budhram.gurung01@gmail.com>
2020-12-30 00:36:13 +0545

    Week 03 modal assignment

--- week3/assignments/w03_02_modal/w03_02.js
+++ week3/assignments/w03_02_modal/w03_02.js
@@ -1,30 +1,29 @@
 'use strict'; // use strict mode for better JS code
 
 // select the elements
+
+// select modal as element with class .modal
 const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnOpenModal = document.querySelector('.show');
-const btnCloseModal = document.querySelector('.close');
+// select overlay as element with class .overlay
+// select btnOpenModal as element with class .show
+// select btnCloseModal as element with class .close
 
 // define the functions
 const openModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+  // remove 'hidden' class to existing classList of modal
+  // remove 'hidden' class to existing classList of overlay
 };
 const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+  // add 'hidden' class to existing classList of modal
+  // add 'hidden' class to existing classList of overlay
 };
 
-// define listeners
-btnOpenModal.addEventListener('click', openModal);
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+// define listener for btnOpenModal on click event and invoke openModal
+// define listener for btnCloseModal on click event and invoke closeModal
+// define listener for overlay on click event and invoke closeModal
 
 // handle escape key event
 document.addEventListener('keydown', function (e) {
-  // console.log(e.key);
-
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
